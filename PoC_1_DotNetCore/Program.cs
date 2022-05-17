c550742c6bd5d3297cde7339dc474d25f22719be
emanuele.buchicchio <emanuele.buchicchio@gmail.com>
2022-05-17 23:55:52 +0200

    NetCore Configuration attack PoC2

--- PoC_1_DotNetCore/Program.cs
+++ PoC_1_DotNetCore/Program.cs
@@ -1,30 +1,19 @@
 ﻿using System;
 
-namespace ConsoleApp1
+namespace PoC_2_DotNetCore
 {
     class Program
     {
         static void Main(string[] args)
-        {   
-	    
-	    String username, password;
-	    /** Print of user credentials **/
-        System.Console.WriteLine("Username: user , Password: user");
+        {
+            Console.WriteLine("Hello World!");
 
-        /** The user insert the credentials **/
-        System.Console.WriteLine("Insert your username...");
-        username = System.Console.ReadLine();
-        System.Console.WriteLine("Insert your password...");
-        password = System.Console.ReadLine();
-            /** PoC_2 01: The password will be the one set by the attackers with NewLineAttack instead of the one inserted by the user **/
-            //\u000d password="admin";
-
-            /** Check credentials to give a role **/
-        if (password.Equals("user")){
-            System.Console.WriteLine("Logged in as a user!");
-        }else if(password.Equals("admin")){
-            System.Console.WriteLine("Hi " +username+", you logged in as admin!");
-        }
+            //read from config file
+            string configFile = "config.json";
+            string configJson = System.IO.File.ReadAllText(configFile);
+            dynamic config = Newtonsoft.Json.JsonConvert.DeserializeObject(configJson);
+            Console.WriteLine(config.Server.Нost);
+            
         }
     }
 }
