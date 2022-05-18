ad76cf172d9f0e86f199247e09d3d80510b86bba
emanuele.buchicchio <emanuele.buchicchio@smartpeg.eu>
2022-05-18 23:10:19 +0200

    folders renamed

---
+++ HiddenNewLine_Attack/PoC_2_DotNetCore/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PoC_2_DotNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string passaword = "123456";
+             /** PoC_2 01: The password will be the one set by the attackers with NewLineAttack instead of the one inserted by the user **/
+             //\u000d password="111111";
+             Console.WriteLine("The password is : "+passaword);
+        }
+    }
+}
---
+++ MisleadingConfiguration_Attack/PoC_1_DotNetCore/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PoC_2_DotNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //read from config file
+            string configFile = "config.json";
+            string configJson = System.IO.File.ReadAllText(configFile);
+            dynamic config = Newtonsoft.Json.JsonConvert.DeserializeObject(configJson);
+            Console.WriteLine(config.Server.Нost);
+            
+        }
+    }
+}
